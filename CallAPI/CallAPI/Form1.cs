e59176447b99d32fbbd656ac740d60b9df7df3df
dunghenry <trandungksnb6930082001>
2022-11-20 23:27:30 +0700

    add call api

---
+++ CallAPI/CallAPI/Form1.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+namespace CallAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            string link = "http://localhost/data/api/sanpham";
+            HttpWebRequest request = WebRequest.CreateHttp(link);
+            WebResponse response = request.GetResponse();
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(SanPham[]));
+            object data = js.ReadObject(response.GetResponseStream());
+            SanPham[] arr = data as SanPham[];
+            dataGridView1.DataSource = arr;
+        }
+    }
+}
---
+++ CallAPI/CallAPI/SanPham.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallAPI
+{
+    //[DataContract]
+    public class SanPham
+    {
+        public int Ma { get; set; }
+       
+        public string Ten { get; set; }
+    
+        public int DonGia { get; set; }
+    }
+}
